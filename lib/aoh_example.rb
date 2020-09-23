d080aa28e9914e4cf86dc57135e9911c603698cc
Joy Evans <joyevans@me.com>
2020-09-23 18:45:03 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -21,7 +21,13 @@ def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
   
-  result = [result.sort]
+  result = [
+    DON_G = { name:  "Don Gately", occupation: "Live-in Staff" },
+    JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+    PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" },
+    KATE_G = { name:  "Kate Gompert", occupation: "None" },
+    BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" },
+    ]
   
 end
 
